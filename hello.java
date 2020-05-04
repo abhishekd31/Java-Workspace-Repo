6e6b1dd4d7a0ae7e391163396406c765a345fb3d
Abhishek Das <abhishek.das22786@gmail.com>
2020-05-04 18:19:55 -0400

    commit

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("This is the first code on VS Editor - Git");
+        System.out.println("Java Programming with VS Code");
         
     }
 }
